dca761a1486363903632f5140364984fb4a0311e
Sean Martin <sean@seanmartinstudio.com>
2022-09-29 13:59:34 -0700

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username:"Sean Martin", password_digest: "12345", title: "Developer")
